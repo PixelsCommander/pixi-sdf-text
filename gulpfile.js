023af76e89539db55f02b51ed1e325334863e6ed
PixelsCommander <denis.radin@gmail.com>
2017-02-18 19:05:48 +0100

    Added CI

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+
+var gulp = require('gulp');
+var ftp = require('vinyl-ftp');
+var gutil = require('gulp-util');
+var minimist = require('minimist');
+
+var args = minimist(process.argv.slice(2));
+
+gulp.task('deploy', function() {
+    var remotePath = '/var/www/sites_user/data/www/pixelscommander.com/polygon/pixi-sdf-text';
+    var conn = ftp.create({
+        host: 'pixelscommander.com',
+        user: args.user,
+        password: args.password,
+        log: gutil.log
+    });
+    gulp.src([
+        './**/*.*',
+        '!./.*',
+        '!./node_modules/**/*.*'
+    ])
+        .pipe(conn.newer(remotePath))
+        .pipe(conn.dest(remotePath));
+});
